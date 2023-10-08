4262033bbe9ae18164dbc5f3e8c7d6a3aea972e8
Piotr Majcher <majcherpiotr.dev@gmail.com>
2023-10-08 17:28:01 +0100

    remove element alternative

--- remove_element.ts
+++ remove_element.ts
@@ -1,6 +1,31 @@
+/*
+Runtime
+Details
+45ms
+Beats 96.29%of users with TypeScript
+Memory
+Details
+44.83MB
+Beats 6.82%of users with TypeScript
+*/
 function removeElement(nums: number[], val: number): number {
   nums.sort((_, b) => b === val ? -1 : 0); 
   let k = nums.length;
   while(nums[k-1] === val) k--;
   return k;
+};
+
+function removeElement2(nums: number[], val: number): number {
+  let deleted = 0;
+  let originalSize = nums.length;
+
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] === val) {
+      nums.splice(i, 1);
+      deleted++;
+      i--;
+    }
+  }
+
+  return originalSize - deleted;
 };
