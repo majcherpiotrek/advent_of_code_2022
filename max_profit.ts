e34262b959adc8d017a6fca3c3709d2d298f33a4
Piotr Majcher <majcherpiotr.dev@gmail.com>
2023-10-09 16:44:37 +0100

    max profit 2 leetcode

--- max_profit.ts
+++ max_profit.ts
@@ -12,5 +12,32 @@ function maxProfit(prices: number[]): number {
 
 }
 
+function maxProfit2(prices: number[]): number {
+  let profit = 0;
+  let buyDay = 0;
+  let sellDay = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < prices[buyDay]) {
+      buyDay = i;
+    }
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4]))
+    if (prices[i] > prices[sellDay]) {
+      sellDay = i;
+    }
+
+    if (sellDay < buyDay) {
+      sellDay = buyDay;
+    }
+
+    const sellProfit = prices[sellDay] - prices[buyDay];
+
+    if (sellProfit > 0) {
+      profit += sellProfit;
+      buyDay = sellDay;
+    }
+  }
+
+  return profit;
+}
+
+console.log(maxProfit2([7,6,4,3,1]))
